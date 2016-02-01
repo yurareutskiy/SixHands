95848f800537e1f0320dbc0c1ca624ca3126e3f7
yurareutskiy <yurareutskiy@gmail.com>
2016-02-01 19:11:52 +0300

    Initial Commit

---
+++ SixHands/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SixHands
+//
+//  Created by Reutskiy Jury on 2/1/16.
+//  Copyright © 2016 Styleru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
