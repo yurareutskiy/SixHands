3b336fccae159ed21701b3663c0426439481aca1
Anton <avscherbakov@icloud.com>
2016-02-08 13:40:20 +0300

    last commit

--- SixHands/FilterViewController.m
+++ SixHands/FilterViewController.m
@@ -46,34 +46,45 @@
     
     self.view1.layer.cornerRadius = 15.f;
     [self.view1.layer setMasksToBounds:YES];
+    [self.view1.layer setBorderColor:[UIColor colorWithRed:79.0/238.0 green:197.0/255.0 blue:183.0/255.0 alpha:1.0].CGColor];
+    [self.view1.layer setBorderWidth: .5f];
+    
     self.view2.layer.cornerRadius = 15.f;
     [self.view2.layer setMasksToBounds:YES];
+    [self.view2.layer setBorderColor:[UIColor colorWithRed:79.0/238.0 green:197.0/255.0 blue:183.0/255.0 alpha:1.0].CGColor];
+    [self.view2.layer setBorderWidth: .5f];
+    
     self.view3.layer.cornerRadius = 15.f;
     [self.view3.layer setMasksToBounds:YES];
+    [self.view3.layer setBorderColor:[UIColor colorWithRed:79.0/238.0 green:197.0/255.0 blue:183.0/255.0 alpha:1.0].CGColor];
+    [self.view3.layer setBorderWidth: .5f];
+    
     self.view4.layer.cornerRadius = 15.f;
     [self.view4.layer setMasksToBounds:YES];
+    [self.view4.layer setBorderColor:[UIColor colorWithRed:79.0/238.0 green:197.0/255.0 blue:183.0/255.0 alpha:1.0].CGColor];
+    [self.view4.layer setBorderWidth: .5f];
     
-    [self.aloneRoom.layer setBorderColor:[UIColor colorWithRed:103.0/255.0 green:58.0/255.0 blue:183.0/255.0 alpha:1.0].CGColor];
+    [self.aloneRoom.layer setBorderColor:[UIColor colorWithRed:79.0/238.0 green:197.0/255.0 blue:183.0/255.0 alpha:1.0].CGColor];
     [self.aloneRoom.layer setBorderWidth: .5f];
     [self.aloneRoom.layer setCornerRadius:7.f];
     
-    [self.room1.layer setBorderColor:[UIColor colorWithRed:103.0/255.0 green:58.0/255.0 blue:183.0/255.0 alpha:1.0].CGColor];
+    [self.room1.layer setBorderColor:[UIColor colorWithRed:79.0/238.0 green:197.0/255.0 blue:183.0/255.0 alpha:1.0].CGColor];
     [self.room1.layer setBorderWidth: .5f];
     [self.room1.layer setCornerRadius:7.f];
     
-    [self.room2.layer setBorderColor:[UIColor colorWithRed:103.0/255.0 green:58.0/255.0 blue:183.0/255.0 alpha:1.0].CGColor];
+    [self.room2.layer setBorderColor:[UIColor colorWithRed:79.0/238.0 green:197.0/255.0 blue:183.0/255.0 alpha:1.0].CGColor];
     [self.room2.layer setBorderWidth: .5f];
     [self.room2.layer setCornerRadius:7.f];
     
-    [self.room3.layer setBorderColor:[UIColor colorWithRed:103.0/255.0 green:58.0/255.0 blue:183.0/255.0 alpha:1.0].CGColor];
+    [self.room3.layer setBorderColor:[UIColor colorWithRed:79.0/238.0 green:197.0/255.0 blue:183.0/255.0 alpha:1.0].CGColor];
     [self.room3.layer setBorderWidth: .5f];
     [self.room3.layer setCornerRadius:7.f];
     
-    [self.room4.layer setBorderColor:[UIColor colorWithRed:103.0/255.0 green:58.0/255.0 blue:183.0/255.0 alpha:1.0].CGColor];
+    [self.room4.layer setBorderColor:[UIColor colorWithRed:79.0/238.0 green:197.0/255.0 blue:183.0/255.0 alpha:1.0].CGColor];
     [self.room4.layer setBorderWidth: .5f];
     [self.room4.layer setCornerRadius:7.f];
     
-    [self.room.layer setBorderColor:[UIColor colorWithRed:103.0/255.0 green:58.0/255.0 blue:183.0/255.0 alpha:1.0].CGColor];
+    [self.room.layer setBorderColor:[UIColor colorWithRed:79.0/238.0 green:197.0/255.0 blue:183.0/255.0 alpha:1.0].CGColor];
     [self.room.layer setBorderWidth: .5f];
     [self.room.layer setCornerRadius:7.f];
     
@@ -95,7 +106,6 @@
     //    [self.view addGestureRecognizer:singleFingerTap];
     
     [self.revealViewController.frontViewController.view addGestureRecognizer:singleFingerTap];
-    
 }
 
 -(void)viewDidDisappear:(BOOL)animated {
@@ -103,6 +113,10 @@
     UISwipeGestureRecognizer *swipe = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(revealToggle:)];
     swipe.direction = UISwipeGestureRecognizerDirectionRight;
     [self.revealViewController.frontViewController.view addGestureRecognizer:swipe];
+}
+
+-(UIStatusBarStyle)preferredStatusBarStyle {
+    return UIStatusBarStyleLightContent;
 }
 
 /*
@@ -119,9 +133,11 @@
     if (!view1b) {
         view1b = YES;
         [self.view1 setImage:[UIImage imageNamed:@"animals"] forState:UIControlStateNormal];
+        [self.view1 setBackgroundColor:[UIColor colorWithRed:79.0/238.0 green:197.0/255.0 blue:183.0/255.0 alpha:1.0]];
     } else {
         view1b = NO;
         [self.view1 setImage:[UIImage imageNamed:@"animalsWhite"] forState:UIControlStateNormal];
+        [self.view1 setBackgroundColor:[UIColor clearColor]];
     }
 }
 
@@ -129,9 +145,11 @@
     if (!view2b) {
         view2b = YES;
         [self.view2 setImage:[UIImage imageNamed:@"metro"] forState:UIControlStateNormal];
+        [self.view2 setBackgroundColor:[UIColor colorWithRed:79.0/238.0 green:197.0/255.0 blue:183.0/255.0 alpha:1.0]];
     } else {
         view2b = NO;
         [self.view2 setImage:[UIImage imageNamed:@"metroWhite"] forState:UIControlStateNormal];
+        [self.view2 setBackgroundColor:[UIColor clearColor]];
     }
 }
 
@@ -139,9 +157,11 @@
     if (!view3b) {
         view3b = YES;
         [self.view3 setImage:[UIImage imageNamed:@"sofa"] forState:UIControlStateNormal];
+        [self.view3 setBackgroundColor:[UIColor colorWithRed:79.0/238.0 green:197.0/255.0 blue:183.0/255.0 alpha:1.0]];
     } else {
         view3b = NO;
         [self.view3 setImage:[UIImage imageNamed:@"sofaWhite"] forState:UIControlStateNormal];
+        [self.view3 setBackgroundColor:[UIColor clearColor]];
     }
 }
 
@@ -149,9 +169,11 @@
     if (!view4b) {
         view4b = YES;
         [self.view4 setImage:[UIImage imageNamed:@"friends"] forState:UIControlStateNormal];
+        [self.view4 setBackgroundColor:[UIColor colorWithRed:79.0/238.0 green:197.0/255.0 blue:183.0/255.0 alpha:1.0]];
     } else {
         view4b = NO;
         [self.view4 setImage:[UIImage imageNamed:@"friendsWhite"] forState:UIControlStateNormal];
+        [self.view4 setBackgroundColor:[UIColor clearColor]];
     }
 }
 
@@ -159,15 +181,15 @@
     if (!aloneRoomb) {
         aloneRoomb = YES;
         roomb = NO;
-        [self.aloneRoom setBackgroundColor:[UIColor colorWithRed:103.0/255.0 green:58.0/255.0 blue:183.0/255.0 alpha:1.0]];
-        [self.aloneRoom setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-        [self.room setBackgroundColor:[UIColor whiteColor]];
-        [self.room setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [self.aloneRoom setBackgroundColor:[UIColor colorWithRed:79.0/238.0 green:197.0/255.0 blue:183.0/255.0 alpha:1.0]];
+        [self.aloneRoom setTitleColor:[UIColor colorWithRed:57.0/255.0 green:70.0/255.0 blue:76.0/255.0 alpha:1.0] forState:UIControlStateNormal];
+        [self.room setBackgroundColor:[UIColor clearColor]];
+        [self.room setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     } else {
         aloneRoomb = NO;
         roomb = NO;
-        [self.aloneRoom setBackgroundColor:[UIColor whiteColor]];
-        [self.aloneRoom setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [self.aloneRoom setBackgroundColor:[UIColor clearColor]];
+        [self.aloneRoom setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     }
 
 }
@@ -176,15 +198,15 @@
     if (!room1b) {
         room1b = YES;
         roomb = NO;
-        [self.room1 setBackgroundColor:[UIColor colorWithRed:103.0/255.0 green:58.0/255.0 blue:183.0/255.0 alpha:1.0]];
-        [self.room1 setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-        [self.room setBackgroundColor:[UIColor whiteColor]];
-        [self.room setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [self.room1 setBackgroundColor:[UIColor colorWithRed:79.0/238.0 green:197.0/255.0 blue:183.0/255.0 alpha:1.0]];
+        [self.room1 setTitleColor:[UIColor colorWithRed:57.0/255.0 green:70.0/255.0 blue:76.0/255.0 alpha:1.0] forState:UIControlStateNormal];
+        [self.room setBackgroundColor:[UIColor clearColor]];
+        [self.room setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     } else {
         room1b = NO;
         roomb = NO;
-        [self.room1 setBackgroundColor:[UIColor whiteColor]];
-        [self.room1 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [self.room1 setBackgroundColor:[UIColor clearColor]];
+        [self.room1 setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     }
 }
 
@@ -192,15 +214,15 @@
     if (!room2b) {
         room2b = YES;
         roomb = NO;
-        [self.room2 setBackgroundColor:[UIColor colorWithRed:103.0/255.0 green:58.0/255.0 blue:183.0/255.0 alpha:1.0]];
-        [self.room2 setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-        [self.room setBackgroundColor:[UIColor whiteColor]];
-        [self.room setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [self.room2 setBackgroundColor:[UIColor colorWithRed:79.0/238.0 green:197.0/255.0 blue:183.0/255.0 alpha:1.0]];
+        [self.room2 setTitleColor:[UIColor colorWithRed:57.0/255.0 green:70.0/255.0 blue:76.0/255.0 alpha:1.0] forState:UIControlStateNormal];
+        [self.room setBackgroundColor:[UIColor clearColor]];
+        [self.room setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     } else {
         room2b = NO;
         roomb = NO;
-        [self.room2 setBackgroundColor:[UIColor whiteColor]];
-        [self.room2 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [self.room2 setBackgroundColor:[UIColor clearColor]];
+        [self.room2 setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     }
 }
 
@@ -208,15 +230,15 @@
     if (!room3b) {
         room3b = YES;
         roomb = NO;
-        [self.room3 setBackgroundColor:[UIColor colorWithRed:103.0/255.0 green:58.0/255.0 blue:183.0/255.0 alpha:1.0]];
-        [self.room3 setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-        [self.room setBackgroundColor:[UIColor whiteColor]];
-        [self.room setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [self.room3 setBackgroundColor:[UIColor colorWithRed:79.0/238.0 green:197.0/255.0 blue:183.0/255.0 alpha:1.0]];
+        [self.room3 setTitleColor:[UIColor colorWithRed:57.0/255.0 green:70.0/255.0 blue:76.0/255.0 alpha:1.0] forState:UIControlStateNormal];
+        [self.room setBackgroundColor:[UIColor clearColor]];
+        [self.room setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     } else {
         room3b = NO;
         roomb = NO;
-        [self.room3 setBackgroundColor:[UIColor whiteColor]];
-        [self.room3 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [self.room3 setBackgroundColor:[UIColor clearColor]];
+        [self.room3 setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     }
 }
 
@@ -224,33 +246,33 @@
     if (!room4b) {
         room4b = YES;
         roomb = NO;
-        [self.room4 setBackgroundColor:[UIColor colorWithRed:103.0/255.0 green:58.0/255.0 blue:183.0/255.0 alpha:1.0]];
-        [self.room4 setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-        [self.room setBackgroundColor:[UIColor whiteColor]];
-        [self.room setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [self.room4 setBackgroundColor:[UIColor colorWithRed:79.0/238.0 green:197.0/255.0 blue:183.0/255.0 alpha:1.0]];
+        [self.room4 setTitleColor:[UIColor colorWithRed:57.0/255.0 green:70.0/255.0 blue:76.0/255.0 alpha:1.0] forState:UIControlStateNormal];
+        [self.room setBackgroundColor:[UIColor clearColor]];
+        [self.room setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     } else {
         room4b = NO;
         roomb = NO;
-        [self.room4 setBackgroundColor:[UIColor whiteColor]];
-        [self.room4 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [self.room4 setBackgroundColor:[UIColor clearColor]];
+        [self.room4 setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     }
 }
 
 - (IBAction)room:(UIButton *)sender {
     if (!roomb) {
         roomb = YES;
-        [self.room setBackgroundColor:[UIColor colorWithRed:103.0/255.0 green:58.0/255.0 blue:183.0/255.0 alpha:1.0]];
-        [self.room setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-        [self.room4 setBackgroundColor:[UIColor whiteColor]];
-        [self.room4 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-        [self.room3 setBackgroundColor:[UIColor whiteColor]];
-        [self.room3 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-        [self.room2 setBackgroundColor:[UIColor whiteColor]];
-        [self.room2 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-        [self.room1 setBackgroundColor:[UIColor whiteColor]];
-        [self.room1 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-        [self.aloneRoom setBackgroundColor:[UIColor whiteColor]];
-        [self.aloneRoom setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [self.room setBackgroundColor:[UIColor colorWithRed:79.0/238.0 green:197.0/255.0 blue:183.0/255.0 alpha:1.0]];
+        [self.room setTitleColor:[UIColor colorWithRed:57.0/255.0 green:70.0/255.0 blue:76.0/255.0 alpha:1.0] forState:UIControlStateNormal];
+        [self.room4 setBackgroundColor:[UIColor clearColor]];
+        [self.room4 setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [self.room3 setBackgroundColor:[UIColor clearColor]];
+        [self.room3 setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [self.room2 setBackgroundColor:[UIColor clearColor]];
+        [self.room2 setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [self.room1 setBackgroundColor:[UIColor clearColor]];
+        [self.room1 setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [self.aloneRoom setBackgroundColor:[UIColor clearColor]];
+        [self.aloneRoom setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
         aloneRoomb = NO;
         room1b = NO;
         room2b = NO;
@@ -258,8 +280,8 @@
         room4b = NO;
     } else {
         roomb = NO;
-        [self.room setBackgroundColor:[UIColor whiteColor]];
-        [self.room setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [self.room setBackgroundColor:[UIColor clearColor]];
+        [self.room setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     }
 }
 
